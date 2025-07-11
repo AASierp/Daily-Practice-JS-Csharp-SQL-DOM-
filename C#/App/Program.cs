6823158c2daa710dbc5e87d9aeb7c4dd11d23831
Aaron Sierp <aaronsierp@gmail.com>
2025-07-10 20:37:28 -0400

    day 1 of daily basic refresher

---
+++ C#/App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+
+            Person p = new Person { Name = "Aaron", Age = 38 };
+            p.Greet();
+        }
+    }
+}
